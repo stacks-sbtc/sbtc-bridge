f803d0f3ad22b352c2c9f54c13432578552be4fd
hz <13457698+hozzjss@users.noreply.github.com>
2025-02-20 19:44:55 +0200

    add contracts and pot link to footer (#93)
    * add contracts and pot link to footer
    
    * Update src/comps/footer.tsx
    
    Co-authored-by: Adriano Di Luzio <aldur@users.noreply.github.com>
    
    * always get the latest signers aggregate btc address
    
    * Consider different envs for the btc pot
    
    ---------
    
    Co-authored-by: Adriano Di Luzio <aldur@users.noreply.github.com>

--- src/comps/footer.tsx
+++ src/comps/footer.tsx
@@ -1,6 +1,51 @@
+"use client";
+import { getAggregateKey } from "@/util/get-aggregate-key";
+import { useQuery } from "@tanstack/react-query";
+import { payments } from "bitcoinjs-lib";
 import Image from "next/image";
+import { hexToBytes } from "@stacks/common";
+import { useAtomValue } from "jotai";
+import { bridgeConfigAtom } from "@/util/atoms";
+import getBitcoinNetwork from "@/util/get-bitcoin-network";
+
+const contracts = [
+  {
+    id: "sbtc-registry",
+    title: "Registry",
+  },
+  {
+    id: "sbtc-token",
+    title: "Token",
+  },
+  {
+    id: "sbtc-deposit",
+    title: "Deposit",
+  },
+  {
+    id: "sbtc-withdrawal",
+    title: "Withdrawal",
+  },
+  {
+    id: "sbtc-bootstrap-signers",
+    title: "Signers Bootstrap",
+  },
+];
 
 export default function Footer({ supportLink }: { supportLink?: string }) {
+  const config = useAtomValue(bridgeConfigAtom);
+  const network = getBitcoinNetwork(config.WALLET_NETWORK);
+  const { data: aggregateAddress } = useQuery({
+    queryKey: ["aggregateAddress"],
+    queryFn: async () => {
+      const response = await getAggregateKey();
+
+      const p2tr = payments.p2tr({
+        internalPubkey: hexToBytes(response.slice(2)),
+        network,
+      });
+      return p2tr.address;
+    },
+  });
   return (
     <footer className="w-full flex flex-col items-center justify-center py-10 px-4 bg-white font-Matter">
       <div
@@ -56,6 +101,35 @@ export default function Footer({ supportLink }: { supportLink?: string }) {
           >
             Github
           </a>
+          <a
+            key="pot"
+            href={`${config.PUBLIC_MEMPOOL_URL}/address/${aggregateAddress}`}
+            target="_blank"
+            rel="noreferrer"
+            className="text-black font-light text-sm"
+          >
+            BTC aggregate address
+          </a>
+        </div>
+      </div>
+      <div className="px-4 text-black w-full max-w-[1200px] mt-8 flex justify-between">
+        <div>
+          <h3>Stacks Mainnet Contracts</h3>
+          <ul>
+            {contracts.map((contract) => (
+              <li className="mb-1" key={contract.id}>
+                <a
+                  key={contract.id}
+                  href={`https://explorer.hiro.so/txid/SM3VDXK3WZZSA84XXFKAFAF15NNZX32CTSG82JFQ4.${contract.id}?chain=mainnet`}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-black font-light text-sm"
+                >
+                  {contract.title}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </footer>
