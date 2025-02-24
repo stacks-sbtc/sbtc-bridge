c9f401026e61b30af0d2e4c0f85f7b846e12952d
janniks <janniks@users.noreply.github.com>
2025-02-25 00:01:03 +0800

    fix: add available check

--- src/util/wallet-utils/src/checkWallets.ts
+++ src/util/wallet-utils/src/checkWallets.ts
@@ -3,20 +3,28 @@ const checkXverseProvider = () => {
     window.XverseProviders?.BitcoinProvider || window.BitcoinProvider,
   );
 };
+
+const checkFordefiProvider = () => {
+  return Boolean(window.FordefiProviders?.UtxoProvider);
+};
+
 export enum WalletType {
   xverse = "xverse",
   leather = "leather",
   asigna = "asigna",
+  fordefi = "fordefi",
 }
 export const checkAvailableWallets: () => {
   [key in WalletType]: boolean;
 } = () => {
   const isLeather = !!window.LeatherProvider;
   const isXverse = checkXverseProvider();
+  const isFordefi = checkFordefiProvider();
 
   return {
     leather: isLeather,
     xverse: isXverse,
     asigna: window.top !== window.self,
+    fordefi: isFordefi,
   };
 };
