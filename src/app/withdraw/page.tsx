a1daa4562d384e5178123acbf5e611226e917b36
Bernardo Garciarivas <brgarciarivas@gmail.com>
2025-02-28 08:10:11 -0500

    feat- add basic logic for calling the withdraw contract

--- src/app/withdraw/page.tsx
+++ src/app/withdraw/page.tsx
@@ -1,7 +1,8 @@
 "use server";
 
 import HomeApp from "@/comps/HomeApp";
+import TempWithdraw from "@/comps/temp-withdraw";
 
 export default async function Home() {
-  return <HomeApp />;
+  return <TempWithdraw />;
 }
