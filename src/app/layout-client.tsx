ebf54b90b6f5080a7e03898d0fcfe8245706247e
Bernardo Garciarivas <brgarciarivas@gmail.com>
2025-02-28 23:36:07 -0500

    1 layout client pr for supporting reskin layout changes

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -8,9 +8,12 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@/query/client";
 import { Suspense, useEffect } from "react";
 import { bridgeConfigAtom } from "@/util/atoms";
-import Header from "@/comps/Header";
-import Footer from "@/comps/footer";
+
 import { AsignaSignActionModals } from "@asigna/btc-connect";
+
+import Footer from "@/comps/footer";
+import Header from "@/comps/Header";
+import { usePathname } from "next/navigation";
 
 export default function LayoutClient({
   children,
@@ -19,31 +22,43 @@ export default function LayoutClient({
   children: React.ReactNode;
   config: BridgeConfig;
 }>) {
+  const pathname = usePathname();
+  const isReskin = pathname?.startsWith("/reskin");
   useEffect(() => {
     store.set(bridgeConfigAtom, config);
     // this is a setup step no need to run it twice
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const backgroundColor = isReskin ? "bg-[#272628] " : "bg-white";
+
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <RenderNotifications />
-        <main className="min-w-screen bg-white  flex items-center flex-col min-h-screen ">
+        <main
+          className={`min-w-screen ${backgroundColor}  flex items-center flex-col min-h-screen `}
+        >
           <Suspense fallback={<div>Loading...</div>}>
-            <Header config={config} />
-            {children}
-            <a
-              key="faqs"
-              href="https://docs.stacks.co/concepts/sbtc/sbtc-faq"
-              target="_blank"
-              rel="noreferrer"
-              className="text-white font-light block text-xl font-Matter px-20 py-2 bg-[#fd8341] rounded opacity-90"
-            >
-              FAQs
-            </a>
-            <Footer supportLink={config.SUPPORT_LINK} />
             <AsignaSignActionModals />
+            {isReskin ? (
+              <>{children}</>
+            ) : (
+              <>
+                <Header config={config} />
+                {children}
+                <a
+                  key="faqs"
+                  href="https://docs.stacks.co/concepts/sbtc/sbtc-faq"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-white font-light block text-xl font-Matter px-20 py-2 bg-[#fd8341] rounded opacity-90"
+                >
+                  FAQs
+                </a>
+                <Footer supportLink={config.SUPPORT_LINK} />
+              </>
+            )}
           </Suspense>
         </main>
       </QueryClientProvider>
