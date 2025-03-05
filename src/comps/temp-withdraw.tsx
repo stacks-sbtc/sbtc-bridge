292ff76378220a37bf1571257a9f1be560493fbd
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-05 19:05:47 +0200

    add comment for regtest base58 addresses workaround

--- src/comps/temp-withdraw.tsx
+++ src/comps/temp-withdraw.tsx
@@ -36,6 +36,9 @@ const decodeBitcoinAddressToClarityRecipient = (
   let addressNetwork = isMainnet ? Network.mainnet : Network.regtest;
   // bech32 is different between all three networks
   // base58 is same between regtest and testnet
+  // the validation lib defaults to testnet when using base58 addresses the same address is valid for regtest
+  // so it would break if the passed address is regtest base58 and the network is regtest,
+  // since to it the address is testnet by default, this is basically a workaround for regtest
   const isRegtestBase58 = !addressInfo.bech32 && !isMainnet;
   addressNetwork = isRegtestBase58 ? Network.testnet : addressNetwork;
 
