f9ddd370505c996ee3e3a37a3c8ec877dd5902e9
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-06 16:53:36 +0200

    handle light mode for nav tabs

--- src/app/reskin/components/tabs/nav-tabs.tsx
+++ src/app/reskin/components/tabs/nav-tabs.tsx
@@ -18,11 +18,11 @@ const tabs = [
 export const NavTabs = () => {
   const pathname = usePathname();
   return (
-    <div className="fixed bottom-0 sm:sticky bg-reskin-dark-gray w-full">
-      <div className="flex h-20 flex-row justify-between items-center w-full sm:mx-auto max-w-5xl">
+    <div className="fixed bottom-0 sm:sticky dark:bg-reskin-dark-gray w-full">
+      <div className="flex h-20 flex-row justify-between items-center w-full sm:mx-auto max-w-5xl relative z-10">
         {tabs.map((tab, index) => (
           <Link
-            className={`h-full uppercase flex-1 flex justify-center items-center ${pathname === tab.link ? "border-t-dark-reskin-orange font-bold sm:border-b-dark-reskin-orange" : "border-t-transparent sm:border-b-transparent"} border-t-4 sm:border-b-4 sm:border-t-0 sm:bottom-auto`}
+            className={`h-full uppercase flex-1 flex justify-center items-center ${pathname === tab.link ? "border-t-orange sm:border-b-orange dark:border-t-dark-reskin-orange font-bold dark:sm:border-b-dark-reskin-orange" : "border-t-transparent sm:border-b-transparent"} border-t-4 sm:border-b-4 sm:border-t-0 sm:bottom-auto`}
             key={index}
             href={tab.link}
           >
--- src/app/reskin/history/page.tsx
+++ src/app/reskin/history/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-black">
+    <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold mb-4">History</h1>
       <p className="text-lg text-gray-600">Coming soon...</p>
     </div>
