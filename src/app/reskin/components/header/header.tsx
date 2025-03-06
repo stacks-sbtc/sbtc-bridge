d8f6c3668bdbb3fdabd712253911020e82663351
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-06 14:27:30 +0200

    fix desktop border bottom header

--- src/app/reskin/components/header/header.tsx
+++ src/app/reskin/components/header/header.tsx
@@ -8,7 +8,7 @@ import { ThemeToggler } from "./theme-toggler";
 import Image from "next/image";
 export default function Header() {
   return (
-    <header className="w-full mx-auto gap-4 items-center flex justify-between h-20 border-b-4 border-light-reskin-border-gray px-4 sm:px-6 lg:px-8 dark:border-dark-reskin-border-gray">
+    <header className="w-full mx-auto gap-4 items-center flex justify-between h-20 border-b-4 border-light-reskin-border-gray px-4 sm:px-6 lg:px-8 dark:border-dark-reskin-border-gray sm:border-b">
       <Link href="/reskin" className="flex items-center w-1/3">
         {/* <SbtcBridgeLogo /> */}
         {/* <h1 className="ml-1 text-xl tracking-tight">Bridge</h1> */}
