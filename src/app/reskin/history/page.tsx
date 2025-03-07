6348458ac385568cb77cff0c29ec309dd8d6a11a
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-07 12:59:12 +0200

    add initial footer (#117)
    * add initial footer
    
     use the nav for footer in mobile and use an informational footer in tablets and larger
    
    * fix footer bg color, remove comments,
    
    fix pages heights taking full screen

--- src/app/reskin/history/page.tsx
+++ src/app/reskin/history/page.tsx
@@ -1,6 +1,6 @@
 export default function Page() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center flex-1">
       <h1 className="text-4xl font-bold mb-4">History</h1>
       <p className="text-lg text-gray-600">Coming soon...</p>
     </div>
