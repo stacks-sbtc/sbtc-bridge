52854d687ad4adda8348130b878cb1bf5462ff7e
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-09 07:09:21 +0200

    add spinner to last step in ui

--- src/app/reskin/components/stepper/timeline.tsx
+++ src/app/reskin/components/stepper/timeline.tsx
@@ -4,14 +4,14 @@ import { defineStepper } from "@stepperize/react";
 // import { Button } from "@/components/ui/button";
 // import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-// import { LoadingIndicator } from "../../assets/loading-indicator";
+import { LoadingIndicator } from "../../assets/loading-indicator";
 
 const { useStepper, steps, utils } = defineStepper(
   {
     id: "amount",
     title: "Select Withdrawal Amount",
     description:
-      "How much BTC are you transferring back to BTC? Enter an amount that's above the dust requirement (546 sats)",
+      "How much BTC are you transferring over to sBTC? Enter an amount that’s above the dust requirement (546 sats)",
   },
   {
     id: "address",
@@ -22,30 +22,52 @@ const { useStepper, steps, utils } = defineStepper(
   {
     id: "status",
     title: "Operation Status:",
-    description:
-      "We will confirm the transaction status once the transaction is confirmed.",
+    description: (
+      <>
+        We will confirm the transaction status once the transaction is
+        confirmed.
+        <div className="flex flex-col items-center justify-center mt-4">
+          <LoadingIndicator />
+          {/* tracking-[-0.02em] */}
+          <div className="h-8 flex items-center text-xl leading-normal text-center text-reskin-dark-gray dark:text-midGray uppercase mt-2">
+            Processing
+          </div>
+          <div className="">
+            ( Estimation{" "}
+            <a
+              href="#"
+              className="text-orange dark:text-dark-reskin-orange hover:underline"
+            >
+              5min
+            </a>{" "}
+            )
+          </div>
+        </div>
+      </>
+    ),
   },
 );
 
-function StepTitle({ step,
+function StepTitle({
+  step,
   currentIndex,
   index,
   stepper,
 }: {
-  step: typeof steps[number],
-  currentIndex: number,
-  index: number,
-  stepper: ReturnType<typeof useStepper>,
-
+  step: (typeof steps)[number];
+  currentIndex: number;
+  index: number;
+  stepper: ReturnType<typeof useStepper>;
 }) {
   return (
     <li className="flex items-center gap-4 flex-shrink-0">
       <span
         onClick={() => stepper.goTo(step.id)}
-        className={`flex text-xs size-6 items-center justify-center rounded-full ${index <= currentIndex
-          ? "bg-orange dark:bg-dark-reskin-orange text-timeline-active-step-text"
-          : "bg-timeline-inactive-gray text-timeline-inactive-step-text"
-          }`}
+        className={`flex text-xs size-6 items-center justify-center rounded-full ${
+          index <= currentIndex
+            ? "bg-orange dark:bg-dark-reskin-orange text-timeline-active-step-text"
+            : "bg-timeline-inactive-gray text-timeline-inactive-step-text"
+        }`}
       >
         {index + 1}
       </span>
@@ -59,9 +81,9 @@ function StepContent({
   currentIndex,
   index,
 }: {
-  step: typeof steps[number],
-  currentIndex: number,
-  index: number,
+  step: (typeof steps)[number];
+  currentIndex: number;
+  index: number;
 }) {
   return (
     <div className="flex gap-4">
@@ -73,42 +95,47 @@ function StepContent({
       >
         <Separator
           orientation="vertical"
-          className={`w-[1px] h-[calc(100%+0.55rem)] relative bottom-2 ${index < currentIndex
-            ? "bg-orange dark:bg-dark-reskin-orange"
-            : "bg-timeline-inactive-gray"
-            }`}
+          className={`w-[1px] h-[calc(100%+.65rem)] relative bottom-1 ${
+            index <= currentIndex
+              ? "bg-orange dark:bg-dark-reskin-orange"
+              : "bg-timeline-inactive-gray"
+          }`}
         />
       </div>
-      <div className="flex-1 text-xs leading-tight text-black dark:text-white opacity-60 ml-3 max-w-52">
+      <div className="flex-1 text-xs leading-tight text-black dark:text-white text-opacity-60 ml-3 max-w-52">
         {step.description}
-        {/* <LoadingIndicator /> */}
       </div>
     </div>
-  )
+  );
 }
-
 
 function DepositTimeline() {
   const stepper = useStepper();
 
   const currentIndex = utils.getIndex(stepper.current.id);
   React.useEffect(() => {
-    stepper.goTo('address')
-  }, [])
+    stepper.goTo("address");
+  }, []);
   return (
-    <div className="py-6 px-8 border rounded-2xl border-black border-opacity-20 dark:border-white dark:border-opacity-20 w-96">
-      <h2 className="uppercase text-xl leading-normal text-timeline-active-step-text dark:text-white">Timeline</h2>
-      <ol className="my-4 flex flex-col gap-2 ">
-        {stepper.all.map((step, index, array) => (
-          <React.Fragment key={step.id}>
+    <div className="pt-6 pb-10 px-8 border rounded-2xl border-black border-opacity-20 dark:border-white dark:border-opacity-20 w-96">
+      <h2 className="uppercase text-xl leading-normal text-timeline-active-step-text dark:text-white">
+        Timeline
+      </h2>
+      <ol className="mt-4 flex flex-col gap-3">
+        {stepper.all.map((step, index) => (
+          <div className="flex flex-col gap-1" key={step.id}>
             <StepTitle
               step={step}
               index={index}
               stepper={stepper}
               currentIndex={currentIndex}
             />
-            <StepContent step={step} index={index} currentIndex={currentIndex} />
-          </React.Fragment>
+            <StepContent
+              step={step}
+              index={index}
+              currentIndex={currentIndex}
+            />
+          </div>
         ))}
       </ol>
     </div>
--- src/app/reskin/page.tsx
+++ src/app/reskin/page.tsx
@@ -3,7 +3,6 @@ import DepositTimeline from "./components/stepper/timeline";
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center flex-1">
-      <h1 className="text-4xl font-bold mb-4">Deposit</h1>
       <DepositTimeline />
     </div>
   );
