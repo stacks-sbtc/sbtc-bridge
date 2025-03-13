ddc5beeb9c66205f4a46e52ad2902aa95f899b63
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-13 18:39:04 +0200

    small but use 10 starting chars and 10 ending chars for addresses

--- src/hooks/use-short-address.ts
+++ src/hooks/use-short-address.ts
@@ -3,7 +3,7 @@ import { useMemo } from "react";
 export function useShortAddress(address: string) {
   return useMemo(() => {
     if (address.length > 10) {
-      return `${address.slice(0, 5)}...${address.slice(-5)}`;
+      return `${address.slice(0, 10)}...${address.slice(-10)}`;
     }
     return address;
   }, [address]);
