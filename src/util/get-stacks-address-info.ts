e0172e4857485e22daa5dffc1a110f3847f29792
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-13 18:54:36 +0200

    add nice utility for checking stacks addresses validity

---
+++ src/util/get-stacks-address-info.ts
@@ -0,0 +1,26 @@
+import { AddressVersion, createAddress } from "@stacks/transactions";
+
+export function getStacksAddressInfo(address: string): {
+  address: string;
+  valid: boolean;
+  type: "mainnet" | "testnet" | "unknown";
+} {
+  try {
+    const addressData = createAddress(address);
+    AddressVersion.TestnetSingleSig;
+    const isMainnet =
+      addressData.version === AddressVersion.MainnetMultiSig ||
+      addressData.version === AddressVersion.MainnetSingleSig;
+    return {
+      address,
+      valid: true,
+      type: isMainnet ? "mainnet" : "testnet",
+    };
+  } catch (e) {
+    return {
+      address,
+      valid: false,
+      type: "unknown",
+    };
+  }
+}
