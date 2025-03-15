80a435a6a8dfbe1d01c0e5b5163a97b6d863cbb1
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-15 14:11:07 +0200

    cleaner short address handling

---
+++ src/hooks/use-short-address.tsx
@@ -0,0 +1,35 @@
+import { useState, useMemo } from "react";
+import { useNotifications } from "./use-notifications";
+import { NotificationStatusType } from "@/comps/Notifications";
+
+export function useShortAddress(address: string) {
+  const [isHovered, setIsHovered] = useState(false);
+  const { notify } = useNotifications();
+
+  const onMouseEnter = () => setIsHovered(true);
+  const onMouseLeave = () => setIsHovered(false);
+  const copyOnClick = () => {
+    navigator.clipboard.writeText(address);
+    notify({
+      message: "Address copied to clipboard",
+      type: NotificationStatusType.SUCCESS,
+    });
+  };
+
+  const shortenedAddress = useMemo(() => {
+    if (address.length > 10) {
+      return `${address.slice(0, 10)}...${address.slice(-10)}`;
+    }
+    return address;
+  }, [address]);
+  return (
+    <span
+      className="cursor-copy break-all"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      onClick={copyOnClick}
+    >
+      {isHovered ? address : shortenedAddress}
+    </span>
+  );
+}
