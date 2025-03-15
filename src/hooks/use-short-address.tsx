4ae9e4d233b688bad5bc97070c29dd555c6806e0
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-15 18:50:07 +0200

    use correct max length for address shortener
    Co-authored-by: Matteo Almanza <matteojug@gmail.com>

--- src/hooks/use-short-address.tsx
+++ src/hooks/use-short-address.tsx
@@ -17,7 +17,7 @@ export function useShortAddress(address: string) {
   };
 
   const shortenedAddress = useMemo(() => {
-    if (address.length > 10) {
+    if (address.length > 20) {
       return `${address.slice(0, 10)}...${address.slice(-10)}`;
     }
     return address;
