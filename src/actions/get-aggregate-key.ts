143ed06b9096292cb63f9d5b1050584dd38e78c6
hz <13457698+hozzjss@users.noreply.github.com>
2025-03-18 19:16:04 +0200

    disable deposits if aggregate key is not available

--- src/actions/get-aggregate-key.ts
+++ src/actions/get-aggregate-key.ts
@@ -19,5 +19,9 @@ export async function getAggregateKey() {
     },
   })) as BufferCV;
 
+  if (result.value.length < 64) {
+    throw new Error("Invalid aggregate key");
+  }
+
   return result.value;
 }
