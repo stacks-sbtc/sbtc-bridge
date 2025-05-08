7a3ec27b2448e1a7cbdec7de9d2e51bde702596a
AH <ali.hs.works@gmail.com>
2025-05-08 09:00:00 +0500

    Add 'learn about sBTC' in Deposit and Withdraw tabs in mobile view

--- src/app/(reskin)/components/footer.tsx
+++ src/app/(reskin)/components/footer.tsx
@@ -1,6 +1,7 @@
+import { useIsMobile } from "@/hooks/use-is-mobile";
 import { ArrowUpRightIcon } from "@heroicons/react/24/outline";
 
-export const Footer = () => {
+const DesktopFooter = () => {
   return (
     <div className="hidden md:flex shadow-[0_-4px_28px_0_rgba(0,0,0,0.13)] justify-center dark:bg-dark-reskin-border-gray">
       <div className="w-full max-w-7xl pt-11 pb-14 pl-14">
@@ -21,4 +22,30 @@ export const Footer = () => {
       </div>
     </div>
   );
+};
+
+const MobileFooter = () => {
+  return (
+    <div className="px-6 py-6 w-full max-w-xl">
+      <a
+        href="https://docs.stacks.co/concepts/sbtc"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex flex-col"
+      >
+        <h2 className="text-lg flex gap-2">
+          Learn more about sBTC
+          <ArrowUpRightIcon className="w-4 text-orange dark:text-dark-reskin-orange" />
+        </h2>
+        <p className="text-darkGray dark:text-midGray font-light">
+          Enter into the world of sBTC with these resources
+        </p>
+      </a>
+    </div>
+  );
+};
+
+export const Footer = () => {
+  const isMobile: boolean = useIsMobile();
+  return <>{isMobile ? <MobileFooter /> : <DesktopFooter />}</>;
 };
