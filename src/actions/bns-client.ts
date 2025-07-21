43e707ab6f2a3fdeef314c24a57a79dffd03c86c
AH <ali.hs.works@gmail.com>
2025-07-21 21:35:59 +0500

    Feedback fixes - Removed the dependency on bns-v2-sdk by directly calling the contract to resolve the names - Improved the BNS name validation.

--- src/actions/bns-client.ts
+++ src/actions/bns-client.ts
@@ -1,28 +1,107 @@
 "use server";
+import {
+  ClarityValue,
+  fetchCallReadOnlyFunction,
+  makeRandomPrivKey,
+  getAddressFromPrivateKey,
+  bufferCVFromString,
+  ClarityType,
+  cvToString,
+} from "@stacks/transactions";
 
-import { getOwner, fetchUserOwnedNames } from "bns-v2-sdk";
 import { env } from "@/env";
+import { decodeFQN } from "@/util/bns";
 
 export type NetworkType = "mainnet" | "testnet";
 
-export async function bnsNameToAddress(name: string): Promise<string | null> {
-  const network: NetworkType = getNetwork();
-  return getOwner({
-    fullyQualifiedName: name,
-    network,
-  });
+const BNS_CONTRACT_DEPLOYER_MAINNET = "SP2QEZ06AGJ3RKJPBV14SY1V5BBFNAW33D96YPGZF";
+const BNS_CONTRACT_DEPLOYER_TESTNET = "ST2QEZ06AGJ3RKJPBV14SY1V5BBFNAW33D9SZJQ0M";
+const BNS_V2_CONTRACT_NAME = "BNS-V2";
+
+interface BnsReadOnlyOptions {
+  functionName: string;
+  functionArgs: ClarityValue[];
+  senderAddress: string;
+  network: NetworkType;
 }
 
-export async function addressToBnsName(
-  address: string,
-): Promise<Array<{ name: string; namespace: string }>> {
-  const network: NetworkType = getNetwork();
-  return fetchUserOwnedNames({
-    senderAddress: address,
-    network: network,
+export async function bnsNameToAddress(
+  fullyQualifiedName: string,
+): Promise<string | null> {
+  const { subdomain, namespace, name } = decodeFQN(fullyQualifiedName);
+  if (subdomain) {
+    throw new Error("Cannot check resolution for a subdomain");
+  }
+
+  const network = getNetwork();
+  const randomAddress = generateRandomAddress();
+  const responseCV = await bnsV2ReadOnlyCall(BNS_V2_CONTRACT_NAME, {
+    functionName: "get-owner-name",
+    senderAddress: randomAddress,
+    functionArgs: [bufferCVFromString(name), bufferCVFromString(namespace)],
+    network,
   });
+
+  if (responseCV.type === ClarityType.ResponseOk) {
+    if (responseCV.value.type === ClarityType.OptionalSome) {
+      if (
+        responseCV.value.value.type === ClarityType.PrincipalStandard ||
+        responseCV.value.value.type === ClarityType.PrincipalContract
+      ) {
+        return cvToString(responseCV.value.value);
+      }
+      throw new Error("Owner is not a principal");
+    }
+    if (responseCV.value.type === ClarityType.OptionalNone) {
+      return null;
+    }
+  }
+  throw new Error("Invalid response from contract");
+}
+
+async function bnsV2ReadOnlyCall(
+  contractName: string,
+  options: BnsReadOnlyOptions,
+): Promise<ClarityValue> {
+  const contractAddress = getBnsContractAddress(options.network);
+  try {
+    const response = await fetchCallReadOnlyFunction({
+      contractAddress,
+      contractName,
+      functionName: options.functionName,
+      functionArgs: options.functionArgs,
+      senderAddress: options.senderAddress,
+      network: options.network,
+    });
+
+    if ((response as any).error) {
+      throw new Error((response as any).error);
+    }
+
+    return response;
+  } catch (error: any) {
+    console.error("Call failed:", {
+      error: error.message,
+      network: options.network,
+    });
+    throw error;
+  }
 }
 
 function getNetwork(): NetworkType {
   return env.WALLET_NETWORK !== "mainnet" ? "testnet" : env.WALLET_NETWORK;
+}
+
+function getBnsContractAddress(network: NetworkType): string {
+  return network == "mainnet"
+    ? BNS_CONTRACT_DEPLOYER_MAINNET
+    : BNS_CONTRACT_DEPLOYER_TESTNET;
+}
+
+function generateRandomAddress() {
+  const randomPrivateKey = makeRandomPrivKey();
+  const privateKeyString = randomPrivateKey;
+  const randomAddress = getAddressFromPrivateKey(privateKeyString);
+
+  return randomAddress;
 }
--- src/util/bns.ts
+++ src/util/bns.ts
@@ -1,7 +1,31 @@
-export function isValidBNSName(name: string): boolean {
-  if (name) {
-    const dotIndex = name.indexOf(".");
-    return dotIndex > 0 && dotIndex < name.length - 1; // dot comes in between
+const validBnsChars = /^[a-z0-9\-_]+$/;
+export function isValidBNSName(fullyQualifiedName: string): boolean {
+  if (fullyQualifiedName) {
+    const { subdomain, namespace, name } = decodeFQN(fullyQualifiedName);
+    // fullyQualifiedName with sub domains are not resolved
+    if (subdomain) {
+      return false;
+    }
+    return validBnsChars.test(namespace) && validBnsChars.test(name);
   }
   return false;
+}
+
+export function decodeFQN(fqdn: string): {
+  name: string;
+  namespace: string;
+  subdomain?: string;
+} {
+  const nameParts = fqdn.split(".");
+  if (nameParts.length > 2) {
+    return {
+      subdomain: nameParts[0],
+      name: nameParts[1],
+      namespace: nameParts[2],
+    };
+  }
+  return {
+    name: nameParts[0],
+    namespace: nameParts[1],
+  };
 }
